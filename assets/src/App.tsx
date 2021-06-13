f2fe80e2bd3c30769cae791e342638b37b136d67
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-13 17:09:38 +0000

    Add Redux

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -1,30 +1,34 @@
-import { ChakraProvider, Link } from "@chakra-ui/react"
-import { BrowserRouter as Router, Link as RouterLink, Route, Switch } from "react-router-dom"
+import { ChakraProvider } from "@chakra-ui/react"
+import { Provider } from "react-redux";
+import { BrowserRouter as Router, Link, Route, Switch } from "react-router-dom"
 import { Header } from "./components/Header"
 import { About } from "./pages/About";
 import { Home } from "./pages/Home";
 import { Users } from "./pages/Users";
+import { store } from "./store";
 
 export const App = () => {
     return (
+      <Provider store={store}>
         <ChakraProvider>
-        <Router>
-          <div>
-            <Header />
-            <Switch>
-              <Route path="/about">
-                <About />
-              </Route>
-              <Route path="/users">
-                <Users />
-              </Route>
-              <Route path="/">
-                <Home />
-              </Route>
-            </Switch>
-          </div>
-        </Router>
+          <Router>
+            <div>
+              <Header />
+              <Switch>
+                <Route path="/about">
+                  <About />
+                </Route>
+                <Route path="/users">
+                  <Users />
+                </Route>
+                <Route path="/">
+                  <Home />
+                </Route>
+              </Switch>
+            </div>
+          </Router>
         </ChakraProvider>
+      </Provider>
       );
     
 }
--- assets/src/pages/Home.tsx
+++ assets/src/pages/Home.tsx
@@ -1,3 +1,13 @@
-export const Home = () => (
-    <div>Home</div>
-)
+import { useDispatch, useSelector } from "react-redux"
+import { AppDispatch, EPayloadActions, RootState } from "../store"
+
+export const Home = () => {
+    const logged_in = useSelector<RootState>(state => state.user.logged_in)
+    const dispatch = useDispatch<AppDispatch>()
+    return (
+        <div>
+            <h1>{JSON.stringify(logged_in)}</h1>
+            <button onClick={() => dispatch({ type: EPayloadActions.LOG_USER_IN, payload: { id: 1, email: 'zbigniew.zolnierowicz@gmail.com' } })}>Toggle log in</button>
+        </div>
+    )
+}
