ab3abdf887ac856a9a0fcbca3783bf2c7851b98c
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-13 14:57:30 +0000

    Add react-router

--- assets/src/App.tsx
+++ assets/src/App.tsx
@@ -1,10 +1,30 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, Link } from "@chakra-ui/react"
+import { BrowserRouter as Router, Link as RouterLink, Route, Switch } from "react-router-dom"
 import { Header } from "./components/Header"
+import { About } from "./pages/About";
+import { Home } from "./pages/Home";
+import { Users } from "./pages/Users";
 
 export const App = () => {
     return (
         <ChakraProvider>
+        <Router>
+          <div>
             <Header />
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/users">
+                <Users />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </Router>
         </ChakraProvider>
-    )
+      );
+    
 }
