e23ea5875ebadb454992fdc05bd8e465d2e8721b
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-13 22:28:38 +0000

    Add a TODO

--- assets/src/store/index.ts
+++ assets/src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore, Dispatch } from "@reduxjs/toolkit"
 import { UserActions, LoggedInUserReducer } from "./UserStore"
 
+// TODO: Introduce a store for storing configuration values for things like max amount of points to be given
+
 export const store = configureStore({
     reducer: {
-        user: LoggedInUserReducer
+      user: LoggedInUserReducer
     }
   })
 
