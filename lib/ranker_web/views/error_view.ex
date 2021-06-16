fb6b6c9b08c259c7a759b631358e2c91ac484a24
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2021-06-16 14:23:18 +0200

    Refactor error views

--- lib/ranker_web/views/error_view.ex
+++ lib/ranker_web/views/error_view.ex
@@ -1,27 +1,18 @@
 defmodule RankerWeb.ErrorView do
   use RankerWeb, :view
 
-  # If you want to customize a particular status code
-  # for a certain format, you may uncomment below.
-  # def render("500.html", _assigns) do
-  #   "Internal Server Error"
-  # end
-
-  # By default, Phoenix returns the status message from
-  # the template name. For example, "404.html" becomes
-  # "Not Found".
-
-  def render("403.json", %{message: message, details: details}) do
+  def render(<< error_code :: binary-size(3), ".json" >>, %{message: message, details: details}) do
+    IO.puts("Matched the catch-all")
     %{
-      status: 403,
+      status: error_code,
       message: message,
       details: details
     }
   end
 
-  def render("403.json", %{message: message}) do
+  def render(<< error_code :: binary-size(3), ".json" >>, %{message: message}) do
     %{
-      status: 403,
+      status: error_code,
       message: message
     }
   end
@@ -41,21 +32,6 @@ defmodule RankerWeb.ErrorView do
     }
   end
 
-  def render("401.json", %{message: message, details: details}) do
-    %{
-      status: 401,
-      message: message,
-      details: details
-    }
-  end
-
-  def render("401.json", %{message: message}) do
-    %{
-      status: 401,
-      message: message
-    }
-  end
-
   def render("401.json", %{details: details}) do
     %{
       status: 401,
@@ -71,21 +47,6 @@ defmodule RankerWeb.ErrorView do
     }
   end
 
-  def render("409.json", %{message: message, details: details}) do
-    %{
-      status: 409,
-      message: message,
-      details: details
-    }
-  end
-
-  def render("409.json", %{message: message}) do
-    %{
-      status: 409,
-      message: message
-    }
-  end
-
   def render("409.json", %{details: details}) do
     %{
       status: 409,
@@ -98,21 +59,6 @@ defmodule RankerWeb.ErrorView do
     %{
       status: 409,
       message: "Conflict with the existing resource.",
-    }
-  end
-
-  def render("400.json", %{message: message, details: details}) do
-    %{
-      status: 400,
-      message: message,
-      details: details
-    }
-  end
-
-  def render("400.json", %{message: message}) do
-    %{
-      status: 400,
-      message: message
     }
   end
 
